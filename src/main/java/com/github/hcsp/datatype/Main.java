219a33614cb2825c6d60fbab3ee5eb422e1d167d
༺༽༾ཊ 船长 ཏ༿༼༻ <captaininphw@gmail.com>
2019-06-26 23:19:05 +0800

    Update Main.java (#27)
    Update Main.java (#27)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        double result = a - b;
+        return Math.abs(result) < 0.00001;
     }
 }
