b823b35b23e43fb13140f67433f67e7041aff1a4
frank-0908 <2059326740@qq.com>
2019-07-14 23:34:44 +0800

    实现正确的double比较相等操作

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-      return Double.compare(a,b)==0;
+        float d = (float)a;
+        float g= (float)b;
+        return d == g;
     }
 }
