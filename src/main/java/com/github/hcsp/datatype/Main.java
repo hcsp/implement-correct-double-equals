4c502a00a2e0807f25f4f27a64fc74cfc9cf2b59
JiaXinWang61 <584937529@qq.com>
2019-09-16 17:41:11 +0800

    double比较

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        if (Math.abs(a) - Math.abs(b) < 0.000001 ){
+             a = b;
+        }
+        return true;
     }
 }
