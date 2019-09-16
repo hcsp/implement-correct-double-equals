350749b2eb36be46cf8f0708f51030d48cb32942
JiaXinWang61 <55199085+JiaXinWang61@users.noreply.github.com>
2019-09-16 17:46:47 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -10,7 +10,7 @@ public class Main {
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
         if (Math.abs(a) - Math.abs(b) < 0.000001 ){
-             a = b;
+            
         }
         return true;
     }
