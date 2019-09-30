37be66b6835983162a1c2b8c44e389d9541f8410
Apolo Du <dudueasy@126.com>
2019-10-01 01:42:37 +0800

    fix: method doubleEquals almostly done (#80)
    fix: method doubleEquals almostly done (#80)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+//        System.out.println("a-b = " + (a-b));
+        double delta =  Math.abs(a-b);
+        System.out.println("delta: = " + delta);
+        return delta < 0.00001;
     }
 }
