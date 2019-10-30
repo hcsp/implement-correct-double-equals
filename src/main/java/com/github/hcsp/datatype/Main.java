323f49a8d7934757e719dab26ae04cacad3e0e1e
oylx <oylx2008@163.com>
2019-10-30 15:23:22 +0800

    判断0.1+0.2==0.3

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.datatype;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         // 这里应该输出true，实际输出的却是false
@@ -9,6 +11,6 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        return Math.abs(a-b)<0.0001;
     }
 }
