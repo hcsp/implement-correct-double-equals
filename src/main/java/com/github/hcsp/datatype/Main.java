96bf3ba5012685b15c8c891ce995607dd47faa8e
bridgeren <240146036@qq.com>
2019-12-01 11:46:25 +0800

    fix bug

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.datatype;
 
+
+
 public class Main {
     public static void main(String[] args) {
         // 这里应该输出true，实际输出的却是false
@@ -9,6 +11,7 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+
+        return Math.abs(a-b)<0.0000001?true:false;
     }
 }
