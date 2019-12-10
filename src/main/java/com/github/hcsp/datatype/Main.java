0534043b8b0be6b50ab9eb068ddb0db4c891d042
JaneWangCoA <43361638+JaneWangCOA@users.noreply.github.com>
2019-12-10 15:33:41 +0800

    浮点数比较大小 (#117)
    浮点数比较大小 (#117)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,10 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        if(Math.abs(a-b)<0.0001){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
