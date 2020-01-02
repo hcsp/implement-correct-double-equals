4488ac7d03945adb0f721cba07bbae73d9e94b55
richard1230 <huyongjinnuaa@163.com>
2020-01-02 18:17:36 +0800

    Java的数据类型练习：实现正确的double比较相等操作okokok

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,11 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        if (Math.abs(a-b)<0.0000000001)
+            return true;
+        else
+            return  false;
+
+
     }
 }
