91b9f7aaa6af60a7019efb14f1fb437986424430
chenxiaolani <2524729719@qq.com>
2020-01-04 16:38:22 +0800

    还是类型转换2.1

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -7,6 +7,8 @@ public class Main {
         // 这里应该输出true，实际输出的却是false
         // 请查找、思考并修复doubleEquals方法中的问题
         System.out.println(doubleEquals(0.1 + 0.2, 0.3));
+        System.out.println(doubleEquals(0.1, 0.2));
+        System.out.println(doubleEquals(0.2, 0.1));
     }
 
     // 判断两个double是否相等
