e150e89015d9f141dfabbeb28423beea5206e035
chenxi1916 <583643011@qq.com>
2020-02-09 13:51:26 +0800

    浮点数只能比较大小，最好不用相等来操作，因为有误差

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -2,6 +2,7 @@ package com.github.hcsp.datatype;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(0.1+0.2);
         // 这里应该输出true，实际输出的却是false
         // 请查找、思考并修复doubleEquals方法中的问题
         System.out.println(doubleEquals(0.1 + 0.2, 0.3));
@@ -9,6 +10,6 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        return Math.abs(a-b) < 0.000000001;
     }
 }
