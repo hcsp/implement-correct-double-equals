ac2ae29a4a7c8d6702d094bcb4843af2a319bd11
pathjh <1639925487@qq.com>
2020-02-26 16:04:21 +0800

    浮点数相减

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        double dis=1e-6;
+        return Math.abs(a-b)<dis;
     }
 }
