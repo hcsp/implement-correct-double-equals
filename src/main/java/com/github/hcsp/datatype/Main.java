3b85cf4711f57b3a8d7deecc5a9781f1c0f195df
7952126 <15521608275@163.com>
2020-03-17 21:39:08 +0800

    TRY

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -4,11 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // 这里应该输出true，实际输出的却是false
         // 请查找、思考并修复doubleEquals方法中的问题
-        System.out.println(doubleEquals(0.1 + 0.2, 0.3));
+
+        System.out.println(doubleEquals(0.1F + 0.2F, 0.3F));
+
     }
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
         return a == b;
+
     }
 }
