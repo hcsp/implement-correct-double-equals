c5d79b2095743e53b58db3341aefe6664b317afc
7952126 <15521608275@163.com>
2020-03-18 11:01:03 +0800

    try2

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return Math.abs(a - b )<10e7;
+        return Math.abs(a - b )<10e-9;
 
     }
 }
