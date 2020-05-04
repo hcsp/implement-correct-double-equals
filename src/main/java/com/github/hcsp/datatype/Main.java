93d814b84d2af911f134fdfcec184b4556166e42
lr6 <36.zhibo@gmail.com>
2020-05-04 20:12:27 +0800

    小数不能判断相等

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        double f = 1e7;
+        double f = 1e-7;
         return Math.abs(a - b) < f;
     }
 }
