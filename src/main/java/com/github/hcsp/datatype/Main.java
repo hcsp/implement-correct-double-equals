87240e9d536328a550c0ee64f5794826c204f335
Administrator <756433556@qq.com>
2020-05-15 00:57:17 +0800

    实现正确的double比较相等操作

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.datatype;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         // 这里应该输出true，实际输出的却是false
@@ -9,6 +11,7 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+
+        return Math.abs(a-b) < 1e-16;
     }
 }
