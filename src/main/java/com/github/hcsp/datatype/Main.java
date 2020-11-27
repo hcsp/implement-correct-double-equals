6f6425cd7dfad7eceb6b7b429d38bce49b967fdc
weiranyi <168578203@qq.com>
2020-11-27 14:39:29 +0800

    格式规范

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        if(Math.abs(a-b)<0.0000000001){
-            a=b;
+        if(Math.abs(a-b) < 0.0000000001) {
+            a = b;
         }
         return a == b;
     }
