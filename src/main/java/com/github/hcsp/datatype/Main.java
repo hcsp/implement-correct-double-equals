028295e0a018cc546412ec9857b6c1d788c6b52c
zeq96 <76999207+zeq96@users.noreply.github.com>
2021-01-16 14:56:50 +0800

    完成作业 (#242)
    完成作业 (#242)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.datatype;
 
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
         // 这里应该输出true，实际输出的却是false
@@ -9,6 +11,10 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        NumberFormat nf = NumberFormat.getNumberInstance();
+        nf.setMaximumFractionDigits(2);
+        String aStr = nf.format(a);
+        String bStr = nf.format(b);
+        return aStr.equals(bStr);
     }
 }
