6f90e6033bddd1e40bc4543ead21abc99fa8d45c
yezhiguang <3424421082@qq.com>
2022-03-16 22:34:01 +0800

    修复double比较

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,6 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a - b <= 0.00000000001;
+        return Math.abs(a - b) < 0.0000001;
     }
 }
