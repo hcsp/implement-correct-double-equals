a50b2ed75a33b0daa144b614e499c3d5f9fc41ce
LiDianguang <101635266+LiDianguang@users.noreply.github.com>
2022-03-20 02:46:38 +0800

    Update Main.java (#299)
    Update Main.java (#299)

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        if (Math.abs(a-b) < 0.000001) {
+            return true;
+        }
+        return false;
     }
 }
