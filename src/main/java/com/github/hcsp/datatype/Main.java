fc2f4798309a6f64bdcec621eb4ecc48deb10252
spearhead0428 <hastur1016@gmail.com>
2023-05-18 22:43:54 +0800

    123123

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
     // 判断两个double是否相等
     public static boolean doubleEquals(double a, double b) {
-        int x= Double.compare(a,b);
-        return (x==1);
+        return (Double.compare(a,b);==1);
     }
 }
